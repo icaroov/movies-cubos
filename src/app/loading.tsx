424c99475c4a10008f09386ecc4c6372cc037e6c
Icaro Oliveira <icarovinici@gmail.com>
2023-06-14 10:37:52 -0300

    feat: adds loading component

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import { LoadingSpinner } from './components/LoadingSpinner'
+
+const Loading = () => {
+  return (
+    <div className='spinnerContainer'>
+      <LoadingSpinner />
+    </div>
+  )
+}
+
+export default Loading
